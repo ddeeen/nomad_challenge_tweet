d6189775338221012599e863751d69c16c6dba09
Sumin <naenjo0315@naver.com>
2024-10-02 02:06:36 +0900

    Assignment 26 - admin pannel

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,5 +1,31 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from .models import Tweet, Like
+
+
+class ElonFilter(admin.SimpleListFilter):
+
+    title = "Filter by word - Elon Musk"
+    parameter_name = "elon"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("elon_musk", "Elon Musk"),
+            ("not_elon_musk", "Not Elon Musk"),
+        ]
+
+    def queryset(self, request, tweets):
+        elon_musk = "Elon Musk"
+        elon = self.value()
+        print(elon)
+        if elon:
+            if elon == "elon_musk":
+                return tweets.filter(payload__contains=elon_musk)
+            else:
+                return tweets.exclude(payload__contains=elon_musk)
+        else:
+            return tweets
 
 
 @admin.register(Tweet)
@@ -12,6 +38,16 @@ class TweetAdmin(admin.ModelAdmin):
         "updated_at",
     )
 
+    list_filter = (
+        ElonFilter,
+        "created_at",
+    )
+
+    search_fields = (
+        "payload",
+        "user__username",
+    )
+
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
@@ -21,3 +57,7 @@ class LikeAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
+
+    list_filter = ("created_at",)
+
+    search_fields = ("user__username",)
